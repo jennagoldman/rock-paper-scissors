082bc944a2202c9f4395433a8c4ade8a463d5fd5
Jenna Goldman <jenna.goldman89@gmail.com>
2020-01-15 22:56:12 -0800

    add styling updates

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ import { getRandomThrow } from './get-random-throw.js';
 import { checkResult } from './check-result.js';
 
 const playButton = document.getElementById('play-button');
-
 const tracker = document.getElementById('trackers');
-
 const totalWins = document.getElementById('win-counter');
 const totalLosses = document.getElementById('loss-counter');
 const totalDraws = document.getElementById('draw-counter');
-
 const results = document.getElementById('results');
-
 const userResult = document.getElementById('user-throw');
-
 const computerResult = document.getElementById('computer-throw');
-
 const winOrLose = document.getElementById('win-or-lose');
+
+// Initialize state
+let totalWinsCount = 0;
+let totalLossesCount = 0;
+let totalDrawsCount = 0;
 
 
 
@@ -28,4 +27,19 @@ playButton.addEventListener('click', () => {
     userResult.textContent = playerThrow;
     computerResult.textContent = computerThrow;
     winOrLose.textContent = result;
+
+    if (result === 'win') {
+        totalWinsCount++;
+    } else if (result === 'lose') {
+        totalLossesCount++;
+    } else if (result === 'draw') {
+        totalDrawsCount++;
+    }
+
+    tracker.style.display = 'block';
+    results.style.display = 'block';
+    
+    totalWins.textContent = `${totalWinsCount} WINS`;
+    totalLosses.textContent = `${totalLossesCount} LOSSES`;
+    totalDraws.textContent = `${totalDrawsCount} DRAWS`;
 });
